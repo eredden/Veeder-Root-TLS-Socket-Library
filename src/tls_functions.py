c63ac30d5ea2c5021fd976d0856d9b659f42f814
Evan Redden <redden.evan@gmail.com>
2024-01-24 19:06:00 -0500

    hex values translated into ieee floats

--- src/tls_functions.py
+++ src/tls_functions.py
@@ -1,33 +1,28 @@
 import math
 from tls_socket import tlsSocket
-from tls_socket import tls_parser
+from tls_socket import tls_parser, get_standard_values, hex_to_float
 
-def in_tank_inventory_report(tls: tlsSocket, timeout: int) -> dict:
+def function_201(tls: tlsSocket, tank: str, timeout: int) -> dict:
     """
     Runs function 201 on a given Veeder-Root TLS device and returns a dict with report info.
 
     tls - A socket for a TLS device, should be created with the tlsSocket class.
+    tank - The tank number (ex. 00 for all tanks, 01 for tank one, etc).
     timeout - Time to wait for a response from the socket after executing the command.
     """
 
-    command = "i20100"
+    command = "i201" + tank
 
     raw_output = tls.execute(command, timeout)
     output = tls_parser(raw_output, command)
+    data = get_standard_values(output)
 
-    data = {}
-    data["year"] = int(output[0:2])
-    data["month"] = int(output[2:4])
-    data["day"] = int(output[4:6])
-    data["hour"] = int(output[6:8])
-    data["minute"] = int(output[8:10])
-
-    # split each tank report
-    no_date_data = output[10:-6]
+    # split tank reports
+    data_without_date = output[10:-6]
     split_data = []
     volume_length = 65
 
-    for i, value in enumerate(no_date_data):
+    for i, value in enumerate(data_without_date):
         array_position = math.floor(i / volume_length)
 
         if i % 65 == 0:
@@ -35,7 +30,7 @@ def in_tank_inventory_report(tls: tlsSocket, timeout: int) -> dict:
         else:
             split_data[array_position] = split_data[array_position] + value
 
-    # strip values from within each individual report
+    # split values from within each individual tank report
     for value in split_data:
         tank_number = value[0:2]
         data["tank_" + tank_number] = {}
@@ -43,13 +38,12 @@ def in_tank_inventory_report(tls: tlsSocket, timeout: int) -> dict:
         tank_data = data["tank_" + tank_number]
         tank_data["product_code"] = value[2:3]
         tank_data["tank_status_bits"] = int(value[3:7])
-        tank_data["eight_character_data_fields"] = int(value[7:9])
-        tank_data["volume"] = value[9:17]
-        tank_data["tc_volume"] = value[17:25]
-        tank_data["ullage"] = value[25:33]
-        tank_data["height"] = value[33:41]
-        tank_data["water"] = value[41:49]
-        tank_data["temperature"] = value[49:57]
-        tank_data["water_volume"] = value[57:65]
+        tank_data["volume"] = hex_to_float(value[9:17])
+        tank_data["tc_volume"] = hex_to_float(value[17:25])
+        tank_data["ullage"] = hex_to_float(value[25:33])
+        tank_data["height"] = hex_to_float(value[33:41])
+        tank_data["water"] = hex_to_float(value[41:49])
+        tank_data["temperature"] = hex_to_float(value[49:57])
+        tank_data["water_volume"] = hex_to_float(value[57:65])
 
     return data
