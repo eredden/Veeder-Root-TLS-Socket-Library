adc8807b6ebf451962ed4d7dc367c22a8fd6f3c9
eredden <redden.evan@gmail.com>
2024-04-11 14:53:11 -0400

    added more test cases for hex to float function

--- tests/test_hex_to_float.py
+++ tests/test_hex_to_float.py
@@ -4,8 +4,12 @@ from src.tls_3xx_functions import hex_to_float
 # Verify that hex_to_float() correctly makes IEEE floats from hexadecimal codes.
 class test_hex_to_float(unittest.TestCase):
     def test_hex_to_float(self):
-        hex = "B8D1B717"
-        test_response = hex_to_float(hex)
-        expected_response = -0.0001
+        hex_codes = ["3F800000", "B8D1B717", "C2C7FAE1", "461C4000"]
+        expected  = [       1.0,    -0.0001,     -99.99,    10000.0]
 
-        self.assertEqual(test_response, expected_response)
+        for index in range(0, len(hex_codes)):
+            actual = hex_to_float(hex_codes[index])
+            self.assertEqual(actual, expected[index])
+
+if __name__ == "__main__":
+    unittest.main()
