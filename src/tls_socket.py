d44553532794bc5bdeae0d1fcf18bfac03e2f7aa
eredden <redden.evan@gmail.com>
2024-04-11 18:09:01 -0400

    sharded execute(), made integrity_check() a method

--- src/tls_socket.py
+++ src/tls_socket.py
@@ -41,95 +41,111 @@ class tlsSocket:
         command - The function code you would like to execute. 
         Make sure this is in computer format.
 
-        soh - Start of header for commands. Veeder-Root allows you to change
-        this for tank gauges so it is optional here. Default value is ASCII 001.
+        soh - This has a default value (ASCII code 001) and should only be 
+        changed if your TLS system is set to use a different start of header.
         """
 
         socket = self.socket
-        etx = b"\x03"
+        etx    = b"\x03"
 
-        byte_command  = soh + bytes(command, "utf-8")
-        is_display_command = command[0].isupper()
+        byte_command = soh + bytes(command, "utf-8")
+        is_display   = command[0].isupper()
 
         # Send command and repeatedly receive data in chunks until ETX is found.
         byte_response = b""
+
         retries   = 30
         timeout   = 1
         data_size = 4098
 
         socket.sendall(byte_command)
 
-        for iterator in range(1, retries + 1):
+        for retry_count in range(1, retries + 1):
             time.sleep(timeout)
-            chunk = socket.recv(data_size)
 
+            chunk = socket.recv(data_size)
             byte_response += chunk
 
-            if etx in chunk:
-                break
+            if etx in chunk: break
 
-            if iterator == retries:
-                raise ValueError("Transmission either partially completed" \
-                    " or failed.")
-        
+            if retry_count == retries: 
+                raise ValueError("Transmission failed.")
+    
+        return self.__handle_response(byte_response, 
+                                      byte_command,
+                                      is_display)
+    
+    def __handle_response(self, byte_response: bytes, 
+                          byte_command: bytes, is_display: bool) -> str:
+        """
+        Handles responses from the TLS system after executing a command.
+
+        byte_response - Response from the TLS system.
+
+        byte_command - The command that was executed to get the response.
+
+        is_display - Used to determine if the command uses Display format.
+        """
+
         # The error code checked below is caused by invalid commands.
-        if byte_response == b"\x019999FF1B\x03":
+        invalid_command_error = b"\x019999FF1B\x03"
+
+        if byte_response == invalid_command_error: 
             raise ValueError("Invalid command.")
 
-        if not is_display_command:
-            # Check if the checksum separator is present and in right position.
-            if b"&&" not in byte_response[-7:-5]:
+        # Check checksum position & value if non-Display format command is used.
+        if not is_display:
+            checksum_separator = b"&&"
+            checksum_position  = byte_response[-7:-5]
+
+            if checksum_separator not in checksum_position:
                 raise ValueError("Checksum missing from command response. " \
                     "Transmission either partially completed or failed.")
 
-            if not data_integrity_check(byte_response):
+            if not self.__data_integrity_check(byte_response):
                 raise ValueError("Incorrect checksum, data integrity " \
                     "invalidated.")
         
         # Removes SOH and ETX from being shown in output.
         response = byte_response.decode("utf-8")[1:][:-1]
-        command = byte_command.decode("utf-8")[1:]
+        command  = byte_command.decode("utf-8")[1:]
 
         # Removes the command from being shown in output.
         response = response.replace(command, "")
 
         # Checks for and removes newlines at both ends of output.
-        if response[:4] == "\r\n\r\n":
+        if response[:4]  == "\r\n\r\n": 
             response = response[4:]
-
-        if response[-4:] == "\r\n\r\n":
+        if response[-4:] == "\r\n\r\n": 
             response = response[:-4]
 
         return response
 
-def data_integrity_check(response: bytes) -> bool:
-    """
-    Verifies whether or not a command response retains its integrity
-    after transmission by comparing it against the response checksum.
+    def __data_integrity_check(self, byte_response: bytes) -> bool:
+        """
+        Verifies whether or not a command response retains its integrity
+        after transmission by comparing it against the response checksum.
 
-    response - Full command response up the checksum itself. Must include
-    the start of header, command, response data, and the && separator.
+        response - Full command response up the checksum itself. Must include
+        the start of header, command, response data, and the && separator.
+        """
 
-    checksum - The checksum contained in-between the && separator and the
-    end of transmission character.
-    """
+        response = byte_response.decode()
+        message  = response[:-5]
+        checksum = response[-5:-1]
 
-    response = response.decode()
-    message  = response[:-5]
-    checksum = response[-5:-1]
+        # Calculate the 16-bit binary count of the message.
+        message_int = sum(ord(char) for char in message) & 0xFFFF
 
-    # Calculate the 16-bit binary count of the message.
-    message_int = sum(ord(char) for char in message) & 0xFFFF
+        # Convert message integer to twos complement integer.
+        message_int = message_int + (message_int >> 16)
+        message_int = message_int
 
-    # Convert message integer to twos complement integer.
-    message_int = message_int + (message_int >> 16)
-    message_int = message_int
+        # Convert checksum hexadecimal string into integer.
+        checksum_int = int(checksum, 16)
 
-    # Convert checksum hexadecimal string into integer.
-    checksum_int = int(checksum, 16)
-
-    # Compare sum of checksum and message to expected result.
-    integrity_threshold = "0b10000000000000000"
-    binary_sum = bin(message_int + checksum_int)
-    
-    return bool(binary_sum == integrity_threshold)
+        # Compare sum of checksum and message to expected result.
+        integrity_threshold = "0b10000000000000000"
+        binary_sum = bin(message_int + checksum_int)
+        
+        return bool(binary_sum == integrity_threshold)
