b3281ec58dc365eb97efc0c7ada5783e3c12ece5
eredden <redden.evan@gmail.com>
2024-05-26 10:24:25 -0400

    unit test for split_data()

---
+++ tests/test_split_data.py
@@ -0,0 +1,27 @@
+import unittest
+from src.tls_3xx_functions import split_data
+
+# Verify that split_data() correctly splits apart chunks of data.
+class test_split_data(unittest.TestCase):
+    def test_split_data(self):
+        data = ["abcdefabcdef", 
+                "zyxwzyxw", 
+                "ABCabcABC",
+                "ABCabcABC"]
+        
+        length = [3,
+                  4,
+                  3,
+                  10]
+        
+        expected = [["abc", "def", "abc", "def"], 
+                    ["zyxw", "zyxw"], 
+                    ["ABC", "abc", "ABC"],
+                    ["ABCabcABC"]]
+
+        for index in range(0, len(data)):
+            actual = split_data(data[index], length[index])
+            self.assertEqual(actual, expected[index])
+
+if __name__ == "__main__":
+    unittest.main()
