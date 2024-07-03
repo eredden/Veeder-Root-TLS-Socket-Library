9ab138ca252e5947d702fe7437e0b438bdb0b171
eredden <redden.evan@gmail.com>
2024-07-03 15:40:43 -0400

    added test for invalid commands

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -6,7 +6,7 @@ from datetime import date
 from tls_socket import tlsSocket
 
 class test_tlsSocket(unittest.TestCase):
-    def test_tlsSocket(self):
+    def test_valid_command(self):
         """
         Verify that tlsSocket.execute() and your system return the same date to test communication.
         """
@@ -20,6 +20,18 @@ class test_tlsSocket(unittest.TestCase):
             actual = tls.execute("i10100")[:6]
 
         self.assertEqual(expected, actual)
+    
+    def test_invalid_command(self):
+        """
+        Verify that tlsSocket.execute() handles invalid commands by returning a ValueError.
+        """
+
+        ip   = os.getenv("TLS_IP")
+        port = os.getenv("TLS_PORT")
+
+        with tlsSocket(ip, int(port)) as tls:
+            try:                        tls.execute("test")
+            except ValueError as error: self.assertIsInstance(error, ValueError)
 
 if __name__ == "__main__":
     unittest.main()
